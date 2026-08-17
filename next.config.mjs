72cf7fdd1b7da2ca315cfbf11db9cf687a9315b2
fazy777 <hafizmfaizanali26688@gmail.com>
2026-08-17 20:12:56 +0500

    Configure serverExternalPackages for firebase-admin, mysql2, and stripe

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  serverExternalPackages: ['firebase-admin', 'mysql2', 'stripe'],
 };
 
 export default nextConfig;
